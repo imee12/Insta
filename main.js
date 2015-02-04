736e7118a24be4bd9ef7a0e45b49a33359b3b0dd
Imee Cuison <imeetwelve@gmail.com>
2015-02-03 22:27:53 -0500

    chucknorris

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ instaPage.getEtsy();
 instaPage.getMeme();
 instaPage.getBeer();
 instaPage.getStarWars();
+instaPage.getChuck();
 
 },
 
@@ -224,7 +225,30 @@ getStarWars: function () {
       console.log(error);
     }
   })
+},
+
+getChuck: function () {
+ $.ajax({
+    url: 'http://api.icndb.com/jokes/random/',
+
+    type: 'GET',
+    datatype: 'JSONP',
+    success: function (data) {
+      //console.log(instaPage.memeConfig.baseurl + "_Select_ByTrending";
+      console.log(data);
+      _.each(data, function(item, idx, arr) {
+       //_.each(item, function (currentItem, index, array) {
+            $('.chuck').append('<p>' + item.joke + '</p>');
+
+  });
+//});
+  },
+      error: function (error) {
+      console.log(error);
+    }
+  })
 }
+
 
 
 
